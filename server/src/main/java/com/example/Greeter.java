8c585dc4797ab8ada230f997f5da8e5756a01f9d
unknown <mkeshvam@gmail.com>
2018-10-09 10:03:48 -0700

    Updated Final Field

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
 
   /*** inherit document
   ****/
-  final public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
